6f891844080d5c365a10004939c6d99eb25c4eae
PlebianJim <108274011+PlebianJim@users.noreply.github.com>
2023-02-07 13:04:40 +0000

    a simple clock face that shows the time

---
+++ main.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.second-hand'); // creates a variable and assigns i the value that is contained within the "second-hand" class
+const minsHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() { //create a new function
+    const now = new Date(); // create a variable and assign it the JS object Date
+
+    const seconds = now.getSeconds(); // create a variable and pass it the value that is stored in the the Date objects getSeconds function
+    const secondsDegrees = ((seconds / 60) * 360) + 90; // create variable and pass it the value of the function getSeconds. Apply maths to trnaslate to a round visual object
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; // update the value of secondHand with the value that is applied in the secondsDegrees just above
+
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) +_ ((mins / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`; 
+}
+
+setInterval(setDate, 1000);
+setDate();
