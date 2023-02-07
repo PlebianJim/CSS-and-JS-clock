227ad642e1afbc5651bfe935cc8554720b2411eb
PlebianJim <108274011+PlebianJim@users.noreply.github.com>
2023-02-07 13:50:30 +0000

    updating script to reflect 360 degrees correctly

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ function setDate() { //create a new function
 
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    const minsDegrees = ((mins / 60) * 360) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) +_ ((mins / 60) * 30) + 90;
+    const hourDegrees = ((hour / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`; 
 }
 
